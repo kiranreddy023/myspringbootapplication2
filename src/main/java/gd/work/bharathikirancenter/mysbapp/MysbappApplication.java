66959b22daec07b797c140a5c75d26b0b7d4cc18
KIRAN KUMAR V <109744725+kiranreddy023@users.noreply.github.com>
2024-05-07 01:04:31 +0530

    Initial Commit

---
+++ src/main/java/gd/work/bharathikirancenter/mysbapp/MysbappApplication.java
@@ -0,0 +1,13 @@
+package gd.work.bharathikirancenter.mysbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysbappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gd/work/bharathikirancenter/mysbapp/MysbappApplicationTests.java
@@ -0,0 +1,13 @@
+package gd.work.bharathikirancenter.mysbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
